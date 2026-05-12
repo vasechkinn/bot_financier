9a4dbf7456f04badbf052cf5b7c3a81a433e8e56
vasechkin <vasechkin@gmail.com>
2026-05-13 01:47:51 +0300

    feat: user table

---
+++ models/user.py
@@ -0,0 +1,14 @@
+from sqlalchemy import String
+from sqlalchemy.orm import (
+    Mapped,
+    mapped_column
+)
+from database import Base
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    tg_id: Mapped[int] = mapped_column(unique=True, nullable=False)
+    login: Mapped[str | None] = mapped_column(String(256))
+    password: Mapped[str | None] = mapped_column(String(256))
