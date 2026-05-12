4dda2a90edbdb9b46955e14e65c088b1b4e919cc
vasechkin <vasechkin@gmail.com>
2026-05-13 01:41:28 +0300

    fix: имя файла и функции

--- models/database.py
+++ models/database.py
@@ -2,14 +2,14 @@ from sqlalchemy.ext.asyncio import (
     create_async_engine,
     async_sessionmaker,
     AsyncSession)
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import DeclarativeBase
 
-DB_URL = 'sqlire+aiosqlite:///database.sqlite3'
+DB_URL = 'sqlite+aiosqlite:///database.sqlite3'
 
 engine = create_async_engine(DB_URL, echo=False)
 sessionLocal = async_sessionmaker(engine, expire_on_commit=False)
 
-class Base(declarative_base):
+class Base(DeclarativeBase):
     pass
 
 async def get_db():
