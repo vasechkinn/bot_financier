b719eb3a61482b1efcde2d352db8b9e284f75cb4
vasechkin <vasechkin@gmail.com>
2026-05-23 01:05:46 +0300

    feat: new router

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import asyncio
 import logging
 from aiogram import Bot, Dispatcher
+from aiogram.fsm.storage.memory import MemoryStorage 
+from handlers.register_handler import router_register
+from midlewares.reg import RegMidleware
 from models.database import engine, Base
 from handlers.income_expense import router
 from handlers.goals_handler import router_goal
+from handlers.register_handler import router_register
+
 async def init_db():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
@@ -12,10 +17,12 @@ async def main():
     await init_db()
 
     bot = Bot(token="8926554564:AAHFoKZdA3kfrnWLMiNiREwe6hY1CRbFVgw")
-    dp = Dispatcher()
+    storage = MemoryStorage()
+    dp = Dispatcher(storage=storage)
+
     dp.include_router(router)
     dp.include_router(router_goal)
-
+    dp.include_router(router_register)
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
